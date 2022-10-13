2e1bffb613b8377e442c9ad97e23002b83a8c9c0
Mohit Kumar <mk4227525@gmail.com>
2022-10-14 01:30:46 +0530

    modified cors settings

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from db.database import engine
 from routers import user, post, comment
 from fastapi.staticfiles import StaticFiles
 from auth import authentication
-from fastapi.middleware.cors import CORSMiddlewares
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -17,8 +17,8 @@ def root():
 origins = ["http://localhost:3000"]
 
 app.add_middleware(
-    CORSMiddlewares,
-    all_origins=origins,
+    CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
