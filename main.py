5ba77510d44c927038bfec92b2b6e642056923f5
Mohit Kumar <mk4227525@gmail.com>
2022-10-14 01:29:54 +0530

    added cores settings

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from db.database import engine
 from routers import user, post, comment
 from fastapi.staticfiles import StaticFiles
 from auth import authentication
+from fastapi.middleware.cors import CORSMiddlewares
 
 app = FastAPI()
 
@@ -13,10 +14,21 @@ def root():
     return "Hello World"
 
 
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddlewares,
+    all_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(authentication.router)
 app.include_router(comment.router)
+
 app.mount("/images", StaticFiles(directory="images"), name="image")
 
 
